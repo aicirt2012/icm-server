87dbe64c9be8d6e94ddad9c4e946a8e362f44bdd
Felix Michel <felix.michel@tum.de>
2016-04-28 14:00:43 +0200

    fix

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -3,19 +3,43 @@ var router = express.Router();
 var Email = require('./../models/Email');
 
 
-
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     console.log('hello');
 
-   // Email.find().distinct('to.')
+    // Email.find().distinct('to.')
 
 
-    Email.find({}).limit(20).exec(function(err, emails){
+    Email.find({}).exec(function (err, emails) {
         console.log(emails.length);
-        if(err)
+        if (err)
             res.json('error');
-        else
-            res.json(emails);
+        else {
+            var map = new Map();
+
+            emails.forEach(function (email) {
+                email.from.forEach(function (f) {
+                    var set = map.get(f.address);
+                    if (!set) {
+                        set = new Set();
+                        map.set(f.address, set);
+                    }
+                    email.to.forEach(function (t) {
+                        set.add(t.address)
+                    });
+                });
+            });
+
+            var json = {};
+            map.forEach(function(values, k){
+                json[k] = [];
+                values.forEach(function(v){
+                    json[k].push(v);
+                });
+            });
+
+            res.json(json);
+        }
+
     });
 
 
