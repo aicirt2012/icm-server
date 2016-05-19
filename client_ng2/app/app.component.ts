58903c9e00b708608f9a66a6be877a6b3c38c591
Daniel Elsner <daniel.elsner@tum.de>
2016-05-19 14:36:06 +0200

    integrated angular2-material

--- client_ng2/app/app.component.ts
+++ client_ng2/app/app.component.ts
@@ -1,27 +1,51 @@
 import {Component} from '@angular/core';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdSpinner} from '@angular2-material/progress-circle';
+import {MdProgressBar} from '@angular2-material/progress-bar';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
 import {Email} from './emails/email';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><ul class="emaillist"><li *ngFor="let email of emails"><span class="email">{{email.subject}}</span></li></ul>'
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  directives: [MdCheckbox,
+    MD_SIDENAV_DIRECTIVES,
+    MdToolbar,
+    MdButton,
+    MdSpinner,
+    MdProgressBar,
+    MD_CARD_DIRECTIVES,
+    MD_INPUT_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MdIcon,
+    MdRadioButton],
+  providers: [MdIconRegistry, MdRadioDispatcher],
 })
 export class AppComponent {
 
-	emails = EMAILS;
+  emails = EMAILS;
 
 }
 
 var EMAILS: Email[] = [
-	{"messageId": "JyiL3xzcVjPLkQ6Gwua8zQ@notifications.google.com",
-	 "subject": "Anmeldeversuch verhindert",
-	 "html": "<html><head></head><body>ayylmao</body></html>",
-	 "text": "Hi this is a test.",
-	 "date": "2016-05-23"
-	},
-	{"messageId": "zcVjPLkQ6Gwua8zQ@notifications.google.com",
-	 "subject": "rsuch verhindert",
-	 "html": "<html><head></head><body>bits&pieces</body></html>",
-	 "text": "Hi this is a test#2.",
-	 "date": "2016-05-24"
-	}
+  {"messageId": "JyiL3xzcVjPLkQ6Gwua8zQ@notifications.google.com",
+    "subject": "Anmeldeversuch verhindert",
+    "html": "<html><head></head><body>ayylmao</body></html>",
+    "text": "Hi this is a test.",
+    "date": "2016-05-23"
+  },
+  {"messageId": "zcVjPLkQ6Gwua8zQ@notifications.google.com",
+    "subject": "rsuch verhindert",
+    "html": "<html><head></head><body>bits&pieces</body></html>",
+    "text": "Hi this is a test#2.",
+    "date": "2016-05-24"
+  }
 ]
