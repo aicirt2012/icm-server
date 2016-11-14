106984a3a3c83dccbe49136858f8c81bd61b4dc0
Daniel Elsner <daniel.elsner@tum.de>
2016-11-14 10:29:14 +0100

    WIP: Refactor TrelloConnector into class + adds promises

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -4,18 +4,31 @@ import {
   search,
   create
 } from '../helpers/task/TrelloConnector';
+import url from 'url';
+import TrelloConnector from '../helpers/task/TrelloConnector';
 
 function getTrelloLogin(req, res) {
   return login(req, res);
 }
 
 function getTrelloCallback(req, res) {
-  return callback(req, res);
+  const query = url.parse(req.url, true).query;
+  const trelloConnector = new TrelloConnector();
+  trelloConnector.callback(query).then((results) => {
+    res.status(200).send(results);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 }
 
 function getTrelloSearch(req, res) {
-  const params = ['query'];
-  return search(req, res, params);
+  const params = url.parse(req.url, true).query;
+  const trelloConnector = new TrelloConnector();
+  trelloConnector.search(params).then((data) => {
+    res.status(200).send(data);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 }
 
 function postTrelloCreate(req, res) {
--- server/helpers/task/TrelloConnector.js
+++ server/helpers/task/TrelloConnector.js
@@ -3,82 +3,101 @@ import {
 } from 'oauth';
 import url from 'url';
 import config from '../../../config/env';
+import TaskConnector from './TaskConnector';
 
-/* BASIC API ENDPOINTS */
-const requestURL = `${config.trello.baseURL}/OAuthGetRequestToken`;
-const accessURL = `${config.trello.baseURL}/OAuthGetAccessToken`;
-const authorizeURL = `${config.trello.baseURL}/OAuthAuthorizeToken`;
-const loginCallback = `${config.domain}:${config.port}/api/task/callback`;
 
-const oauth = new OAuth(requestURL, accessURL, config.trello.key, config.trello.secret,
-  config.trello.oauthVersion, loginCallback, config.trello.oauthSHA);
+class TrelloConnector extends TaskConnector  {
+  constructor(options) {
+    super(options);
+    this.requestURL = `${config.trello.baseURL}/OAuthGetRequestToken`;
+    this.accessURL = `${config.trello.baseURL}/OAuthGetAccessToken`;
+    this.authorizeURL = `${config.trello.baseURL}/OAuthAuthorizeToken`;
+    this.loginCallback = `${config.domain}:${config.port}/api/task/callback`;
+    this.oauthSecrets = {};
+    this.accessToken = config.trello.accessToken;
+    this.accessTokenSecret = config.trello.accessTokenSecret;
+    this.oauth = new OAuth(this.requestURL, this.accessURL, config.trello.key, config.trello.secret,
+      config.trello.oauthVersion, this.loginCallback, config.trello.oauthSHA);
+  }
 
-// TODO: for production find a better way to store oauthSecrets
-let oauthSecrets = {};
-let accessToken = config.trello.accessToken;
-let accessTokenSecret = config.trello.accessTokenSecret;
-
-const login = function login(req, res) {
-  return oauth.getOAuthRequestToken((error, token, tokenSecret, results) => {
-    oauthSecrets[token] = tokenSecret;
-    res.writeHead(302, {
-      Location: `${authorizeURL}?scope=read,write&oauth_token=${token}&name=${config.trello.appName}`
+  callback(query) {
+    const token = query.oauth_token;
+    const tokenSecret = this.oauthSecrets[token];
+    const verifier = query.oauth_verifier;
+    return new Promise((resolve, reject) => {
+      this.oauth.getOAuthAccessToken(token, tokenSecret, verifier,
+        (error, accessToken, accessTokenSecret, results) => {
+          // TODO: store accessToken and accessTokenSecret somewhere
+          console.log(accessToken);
+          console.log(accessTokenSecret);
+          error ? reject(error) : resolve(results);
+        });
     });
-    return res.end();
-  }, this);
-};
+  }
 
-const callback = function callback(req, res) {
-  const query = url.parse(req.url, true).query;
-  const token = query.oauth_token;
-  const tokenSecret = oauthSecrets[token];
-  const verifier = query.oauth_verifier;
-  return oauth.getOAuthAccessToken(token, tokenSecret, verifier,
-    (error, accessToken, accessTokenSecret, results) => {
-      // TODO: store accessToken and accessTokenSecret somewhere
-      console.log(accessToken);
-      console.log(accessTokenSecret);
-      return res.end();
+  //
+  // const login = function login(req, res) {
+  //   return oauth.getOAuthRequestToken((error, token, tokenSecret, results) => {
+  //     oauthSecrets[token] = tokenSecret;
+  //     res.writeHead(302, {
+  //       Location: `${authorizeURL}?scope=read,write&oauth_token=${token}&name=${config.trello.appName}`
+  //     });
+  //     return res.end();
+  //   }, this);
+  // };
+  //
+
+  //
+  // const create = function create(req, res, params) {
+  //   const path = (url.parse(req.url, true).pathname).slice(7); // without /create
+  //   const queries = ;
+  //   return oauth.getProtectedResource(
+  //     `${config.trello.baseURL}${path}?${addQueries(params, queries)}`,
+  //     "POST", accessToken, accessTokenSecret,
+  //     (error, data, response) => {
+  //       return res.end(data)
+  //     }
+  //   )
+  // }
+  //
+  createTask() {
+    //Logic for creating task
+    return true;
+  }
+
+  updateTask(task) {
+    //Logic for updating task
+    return true;
+  }
+
+  deleteTask(task) {
+    //Logic for deleting task
+    return true;
+  }
+
+  search(params) {
+    const path = '/search';
+    return new Promise((resolve, reject) => {
+      console.log(params);
+      console.log(this.oauth);
+      this.oauth.getProtectedResource(
+        `${config.trello.baseURL}${path}?${this.addQueries(params)}`,
+        "GET", this.accessToken, this.accessTokenSecret,
+        (error, data, response) => {
+          error ? reject(error) : resolve(data);
+        }
+      );
     });
-};
+  }
 
-const search = function search(req, res, params) {
-  const path = url.parse(req.url, true).pathname;
-  const queries = url.parse(req.url, true).query;
-  return oauth.getProtectedResource(
-    `${config.trello.baseURL}${path}?${addQueries(params, queries)}`,
-    "GET", accessToken, accessTokenSecret,
-    (error, data, response) => {
-      return res.end(data)
-    }
-  )
+  addQueries(queries) {
+    let queryString = '';
+    Object.keys(queries).forEach((key) => {
+      queryString += `${key}=${queries[key]}&`;
+    })
+    console.log(queryString);
+    return queryString.slice(0, -1);
+  }
 }
 
-const create = function create(req, res, params) {
-  const path = (url.parse(req.url, true).pathname).slice(7); // without /create
-  const queries = url.parse(req.url, true).query;
-  return oauth.getProtectedResource(
-    `${config.trello.baseURL}${path}?${addQueries(params, queries)}`,
-    "POST", accessToken, accessTokenSecret,
-    (error, data, response) => {
-      return res.end(data)
-    }
-  )
-}
-
-function addQueries(params, queries) {
-  let queryString = '';
-  params.forEach((name) => {
-    if (queries[name]) {
-      queryString += `${name}=${queries[name]}&`;
-    }
-  });
-  return queryString.slice(0, -1);
-}
-
-export default {
-  login,
-  callback,
-  search,
-  create
-};
+export default TrelloConnector;
