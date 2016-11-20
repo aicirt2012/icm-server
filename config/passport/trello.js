846d471730ba8ce141d846d1e65e032c908ae3e4
Daniel Elsner <daniel.elsner@tum.de>
2016-11-20 22:21:38 +0100

    working oauth trello with passport

--- config/passport/trello.js
+++ config/passport/trello.js
@@ -2,19 +2,42 @@ import {
   Strategy as TrelloStrategy
 } from 'passport-trello';
 import config from '../env';
+import User from '../../server/models/user.model';
 
 function verifyTrello(req, token, tokenSecret, profile, done) {
-  // TODO
+  User.find({
+    trelloId: profile.id
+  }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user.length > 0) {
+      return done(null, user[0]);
+    } else {
+      console.log(profile);
+      user = new User();
+      user.trelloId = profile.id;
+      user.username = profile.displayName;
+      user.email = profile.emails[0].value;
+      user.password = profile.id;
+      user.save((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done(null, user)
+      });
+    }
+  });
 }
 
 export function trelloAuthentication(passport) {
   passport.use(new TrelloStrategy({
       consumerKey: config.oauth.trello.consumerKey,
       consumerSecret: config.oauth.trello.consumerSecret,
-      callbackURL: config.oauth.google.callbackURL,
+      callbackURL: config.oauth.trello.callbackURL,
       passReqToCallback: true,
       trelloParams: {
-        scope: 'read,write',
+        scope: 'read,write,account',
         name: config.oauth.trello.appName,
         expiration: 'never'
       }
