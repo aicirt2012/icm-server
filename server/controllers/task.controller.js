d6d9726c58a1448ef5fd0faf4742668aa0af75a2
poulzinho <paul.gualotuna@tum.de>
2016-11-22 03:41:23 +0100

    Add post, put, delete, get Task

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,15 +1,63 @@
-import {
-  login,
-  callback,
-  search,
-  create
-} from '../helpers/task/TrelloConnector';
 import url from 'url';
 import TrelloConnector from '../helpers/task/TrelloConnector';
 import User from '../models/user.model';
 
-function getTrelloLogin(req, res) {
-  return login(req, res);
+function postTrelloCreate(req, res) {
+  User.findOne({
+    username: req.user.username
+  }, (err, user) => {
+    if (err) {
+      res.status(400).send(err);
+      return;
+    } else {
+      const params = req.body;
+      const trelloConnector = new TrelloConnector(user.trello);
+      trelloConnector.createTask(params).then((data) => {
+        res.status(200).send(data);
+      }).catch((err) => {
+        res.status(400).send(err);
+      });
+    }
+  });
+}
+
+function putTrelloUpdate(req, res) {
+  User.findOne({
+    username: req.user.username
+  }, (err, user) => {
+    if (err) {
+      res.status(400).send(err);
+      return;
+    } else {
+      const id = (url.parse(req.url, true).pathname).slice(8);
+      const params = req.body;
+      const trelloConnector = new TrelloConnector(user.trello);
+      trelloConnector.updateTask(id, params).then((data) => {
+        res.status(200).send(data);
+      }).catch((err) => {
+        res.status(400).send(err);
+      });
+    }
+  });
+}
+
+function deleteTrelloDelete(req, res) {
+  User.findOne({
+    username: req.user.username
+  }, (err, user) => {
+    if (err) {
+      res.status(400).send(err);
+      return;
+    } else {
+      const id = (url.parse(req.url, true).pathname).slice(8);
+      const trelloConnector = new TrelloConnector(user.trello);
+      trelloConnector.deleteTask(id).then((data) => {
+        res.status(200).send(data);
+      }).catch((err) => {
+        res.status(400).send(err);
+      });
+    }
+  });
 }
 
 function getTrelloSearch(req, res) {
@@ -31,13 +79,9 @@ function getTrelloSearch(req, res) {
   });
 }
 
-function postTrelloCreate(req, res) {
-  const params = ['due', 'idList'];
-  return create(req, res, params);
-}
-
 export default {
-  getTrelloLogin,
-  getTrelloSearch,
-  postTrelloCreate
+  postTrelloCreate,
+  putTrelloUpdate,
+  deleteTrelloDelete,
+  getTrelloSearch
 };
--- server/helpers/task/TrelloConnector.js
+++ server/helpers/task/TrelloConnector.js
@@ -1,55 +1,91 @@
-import {
-  OAuth
-} from 'oauth';
-import url from 'url';
 import config from '../../../config/env';
 import TaskConnector from './TaskConnector';
 import fetch from 'node-fetch';
 
-class TrelloConnector extends TaskConnector  {
+class TrelloConnector extends TaskConnector {
   constructor(options) {
     super(options);
     this.accessToken = this.options.trelloAccessToken;
     this.accessTokenSecret = this.options.trelloAccessTokenSecret;
   }
 
-  //
-  // const create = function create(req, res, params) {
-  //   const path = (url.parse(req.url, true).pathname).slice(7); // without /create
-  //   const queries = ;
-  //   return oauth.getProtectedResource(
-  //     `${config.trello.baseURL}${path}?${addQueries(params, queries)}`,
-  //     "POST", accessToken, accessTokenSecret,
-  //     (error, data, response) => {
-  //       return res.end(data)
-  //     }
-  //   )
-  // }
-  //
-  createTask() {
-    //Logic for creating task
-    return true;
-  }
-
-  updateTask(task) {
-    //Logic for updating task
-    return true;
-  }
-
-  deleteTask(task) {
-    //Logic for deleting task
-    return true;
-  }
-
-  search(params) {
-    const path = '/search';
+  /**
+   * create task
+   * @params {string} - idList (required).
+   * @params {string} - name (desirable).
+   */
+  createTask(params) {
+    const url = this.buildURL('/cards', params);
     return new Promise((resolve, reject) => {
-      fetch(`${config.trello.baseURL}${path}?key=${config.trello.key}&token=${this.accessToken}&${this.addQueries(params)}`).then((res) => res.json()).then((json) => {
-        resolve(json);
-      }).catch((err) => {
-        reject(err);
-      })
-    });;
+      fetch(url, {method: 'POST', body: ''})
+        .then((res) => res.json())
+        .then((json) => {
+          resolve(json);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    });
+  }
+
+  /**
+   * update task 
+   * @params {string} - name (optional).
+   */
+  updateTask(id, params) {
+    const url = this.buildURL(`/cards/${id}`, params);
+    return new Promise((resolve, reject) => {
+      fetch(url, {method: 'PUT', body: ''})
+        .then((res) => res.json())
+        .then((json) => {
+          resolve(json);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    });
+  }
+
+  /**
+   * delete tasks
+   */
+  deleteTask(id) {
+    const url = this.buildURL(`/cards/${id}`, '');
+    return new Promise((resolve, reject) => {
+      fetch(url, {method: 'DELETE', body: ''})
+        .then((res) => res.json())
+        .then((json) => {
+          resolve(json);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    });
+  }
+
+  /**
+   * search tasks
+   * @params {string} - query (required).
+   */
+  search(params) {
+    const url = this.buildURL('/search', params);
+    return new Promise((resolve, reject) => {
+      fetch(url).then((res) => res.json())
+        .then((json) => {
+          resolve(json);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    });
+  }
+
+  /* Trello Connector utils */
+  buildURL(path, params) {
+    return `${config.trello.baseURL}${path}?`
+      + `key=${config.trello.key}&`
+      + `token=${this.accessToken}&`
+      + `${this.addQueries(params)}`;
   }
 
   addQueries(queries) {
@@ -57,7 +93,6 @@ class TrelloConnector extends TaskConnector  {
     Object.keys(queries).forEach((key) => {
       queryString += `${key}=${queries[key]}&`;
     })
-    console.log(queryString);
     return queryString.slice(0, -1);
   }
 }
