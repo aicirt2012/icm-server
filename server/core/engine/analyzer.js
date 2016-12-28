01602d6f77b6dc3d2b31c2e8eaccbe66dd005fc0
poulzinho <paul.gualotuna@tum.de>
2016-12-28 15:16:14 +0100

    WIP: Fetch connected tasks for email

--- server/core/engine/analyzer.js
+++ server/core/engine/analyzer.js
@@ -1,26 +1,42 @@
 import Promise from 'bluebird';
 import moment from 'moment';
+import taskCtrl from '../controllers/task.controller';
 
 class Analyzer {
 
+  // START: Email passend into Analyzer
   constructor(email) {
     this.email = email;
   }
 
-  // START: Email passend into Analyzer
-  // Fetch connected tasks for Email (sofern vorhanden) [{taskId:, provider:}] -> forEach fetch entire task object from trello (GET :taskId)
-  // Extract suggestion out of email object -> NEW TASK({emailsubject, emailContent, emailRecipient}) (HOW CAN EMAIL FIELDS BE MAPPED TO TRELLO FIELDS) -> subject -> name, emailContent -> desc (in trello-card object)
+  // Fetch connected tasks for Email (sofern vorhanden) [{taskId:, provider:}] -> 
+  // forEach fetch entire task object from trello (GET :taskId)
+  fetchConnectedTasks() {
+    let connectedTasks = [];
+    this.email.tasks.forEach(t => {
+      // if or switch(t.provider)... 
+      let task = taskCtrl.getTask(t.id); // t.taskId
+      connectedTasks.append(task);
+    })
+  }
+
+  // Extract suggestion out of email object -> 
+  // NEW TASK({emailsubject, emailContent, emailRecipient}) 
+  // (HOW CAN EMAIL FIELDS BE MAPPED TO TRELLO FIELDS) -> 
+  // subject -> name, emailContent -> desc (in trello-card object)
+  detectPattern() {
+    this.email['suggestedTasks'] = [{name: "peters favorite band", desc: "is coldplay"}];
+    this.email['linkedTasks'] = [{name: "coldplay is peters favorite", desc: "band"}];
+    return this.email;
+  }
+
   // END: Email + conncetedTasks + suggestion aus Analyzer -> {subject, emailContent, suggestedTasks:[{name, desc...}], linkedTasks:[{name, desc}]}
 
-  detectPattern() {
-      this.email['suggestedTasks'] = [{name:"peters favorite band", desc:"is coldplay"}];
-      this.email['linkedTasks'] = [{name:"coldplay is peters favorite", desc:"band"}];
-      return this.email;
-  }
 
-  createSuggestedTask() {
-  }
-
+  // Maybe it is better to show suggestions in GUI first, and only add them when the user
+  // accepts those
+  // createSuggestedTask() {
+  // }
 
 }
 export default Analyzer;
