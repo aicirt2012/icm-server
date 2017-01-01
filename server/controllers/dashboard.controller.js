87f086b01a57fda6f1b2792dafd2f19215a9933d
poulzinho <paul.gualotuna@tum.de>
2017-01-01 17:02:36 +0100

    Add one user --temporary fix until we define if dashboard is embedded in Webapp where there is a current user

--- server/controllers/dashboard.controller.js
+++ server/controllers/dashboard.controller.js
@@ -1,54 +1,56 @@
 import Email from '../models/email.model';
 import mongoose from 'mongoose';
+import User from '../models/user.model';
 const ObjectId = mongoose.Types.ObjectId;
 
-function getMonthlyPunchCard(userId){
+function getMonthlyPunchCard(userId) {
   return Email.aggregate([
-    {$match: { user: ObjectId(userId)}},
-    {$project: {
-        month: { $month: "$date" },
-        day: { $dayOfMonth: "$date" }
-      }
-    },
-    {$group: {_id: {month: '$month', day: '$day'}, count: {$sum:1}}},
-    {$sort:{'_id.month':1, '_id.day':1}},
-    {$project: {_id: 0, month: '$_id.month', day: '$_id.day', count:1}}
-  ]);
-}
-
-function getDailyPunchCard(userId){
-  return Email.aggregate([
-    {$match: { user: ObjectId(userId)}},
+    {$match: {user: ObjectId(userId)}},
     {
       $project: {
-        day: { $dayOfWeek: "$date" },
-        hour: { $hour: "$date" },
+        month: {$month: "$date"},
+        day: {$dayOfMonth: "$date"}
       }
     },
-    {$group: {_id: {day: '$day', hour: '$hour'}, count: {$sum:1}}},
-    {$sort:{'_id.day':1, '_id.hour':1}},
-    {$project: {_id: 0, day: '$_id.day', hour: '$_id.hour', count:1}}
+    {$group: {_id: {month: '$month', day: '$day'}, count: {$sum: 1}}},
+    {$sort: {'_id.month': 1, '_id.day': 1}},
+    {$project: {_id: 0, month: '$_id.month', day: '$_id.day', count: 1}}
   ]);
 }
 
-function getTopSender(userId){
+function getDailyPunchCard(userId) {
   return Email.aggregate([
-    {$match: { user: ObjectId("5836fe8a14272b01c404257c")}},
+    {$match: {user: ObjectId(userId)}},
+    {
+      $project: {
+        day: {$dayOfWeek: "$date"},
+        hour: {$hour: "$date"},
+      }
+    },
+    {$group: {_id: {day: '$day', hour: '$hour'}, count: {$sum: 1}}},
+    {$sort: {'_id.day': 1, '_id.hour': 1}},
+    {$project: {_id: 0, day: '$_id.day', hour: '$_id.hour', count: 1}}
+  ]);
+}
+
+function getTopSender(userId) {
+  return Email.aggregate([
+    {$match: {user: ObjectId(userId)}},
     {$unwind: '$from'},
-    {$group: {_id: {address: '$from.address', name: '$from.name'}, count: {$sum:1}}},
-    {$sort: {count:-1}},
-    {$project: {_id: 0, address: '$_id.address', name: '$_id.name', count:1}}
+    {$group: {_id: {address: '$from.address', name: '$from.name'}, count: {$sum: 1}}},
+    {$sort: {count: -1}},
+    {$project: {_id: 0, address: '$_id.address', name: '$_id.name', count: 1}}
   ]);
 }
 
 
-function getTopReceiver(userId){
+function getTopReceiver(userId) {
   return Email.aggregate([
-    {$match: { user: ObjectId(userId)}},
+    {$match: {user: ObjectId(userId)}},
     {$unwind: '$to'},
-    {$group: {_id: {address: '$to.address', name: '$to.name'}, count: {$sum:1}}},
-    {$sort: {'count':-1}},
-    {$project: {_id: 0, address: '$_id.address', name: '$_id.name', count:1}}
+    {$group: {_id: {address: '$to.address', name: '$to.name'}, count: {$sum: 1}}},
+    {$sort: {'count': -1}},
+    {$project: {_id: 0, address: '$_id.address', name: '$_id.name', count: 1}}
   ]);
 }
 
@@ -56,31 +58,36 @@ function getSummary(req, res) {
 
   //TODO may consider only the last 12 month not all data ...
   //TODO differentiate between send, received and all mails
-  //TODO use id of current user
-  const userId = "5836fe8a14272b01c404257c";
 
-  const s = {
-    punchcard:{},
-    network:{},
-    structural:{}
-  };
+  //TODO use id of current user?? assuming that the dashboard is embedded in WebApp??
+  User.findOne().then((user, err) => {
 
-  getMonthlyPunchCard(userId)
-    .then((monthlySummary)=>{
-      s.punchcard.monthly = monthlySummary;
-      return getDailyPunchCard(userId);
-    })
-    .then((dailySummary)=>{
-      s.punchcard.daily = dailySummary;
-      return getTopSender(userId)
-    })
-    .then((topSender)=>{
-      s.network.topsender = topSender;
-      return getTopReceiver(userId)
-    })
-    .then((topReceiver)=>{
-      s.network.topreceiver = topReceiver;
-      res.status(200).send(s);
+    const userId = user._id;
+
+    const s = {
+      punchcard: {},
+      network: {},
+      structural: {}
+    };
+
+    getMonthlyPunchCard(userId)
+      .then((monthlySummary) => {
+        s.punchcard.monthly = monthlySummary;
+        return getDailyPunchCard(userId);
+      })
+      .then((dailySummary) => {
+        s.punchcard.daily = dailySummary;
+        return getTopSender(userId)
+      })
+      .then((topSender) => {
+        s.network.topsender = topSender;
+        return getTopReceiver(userId)
+      })
+      .then((topReceiver) => {
+        s.network.topreceiver = topReceiver;
+        res.status(200).send(s);
+      });
+
   });
 
 }
