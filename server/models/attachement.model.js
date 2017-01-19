4fe5529d4d5c779b8734427221cc458a0e0934a9
Felix Michel <felix.michel@tum.de>
2017-01-19 11:18:03 +0100

    attachment fix

--- server/models/attachement.model.js
+++ server/models/attachement.model.js
@@ -1,47 +1,49 @@
 import mongoose from 'mongoose';
 import gridfs from 'mongoose-gridfs';
 
-
-const Attachment = gridfs({
-  collection:'attachments',
-  model:'Attachment'
-}).model;
+function Attachment() {
+  return gridfs({
+    collection: 'attachments',
+    model: 'Attachment'
+  }).model;
+}
 
 /**
  * @param fileName of attachment
  * @param contentType to deliver via http e.g. 'text/plain', ...
- * @param rs readstream e.g. fs.createReadStream('D:/test.txt')
- * @returns Promise that returns meta data of the created attachement
+ * @param rs readStream e.g. fs.createReadStream('D:/test.txt')
+ * @returns Promise that returns meta data of the created attachment
  */
 function createAttachment(fileName, contentType, rs){
   return new Promise((resolve, reject)=>{
-    Attachment.write({
+    Attachment().write({
         filename: fileName,
         contentType: contentType
       },
       rs,
-      (err, attachement)=> {
+      (err, attachment)=> {
         if(err){
           reject()
         }else{
-          resolve(attachement)
+          resolve(attachment)
         }
       }
     );
   });
+
 }
 
 function deleteAttachment(id){
   //TODO implement this
 }
 
-function findAttchment(id){
-  //TODO implement this
+function findAttachment(id){
+  return Attachment.readById(id);
 }
 
 
 export default {
   create: createAttachment,
-  find: findAttchment,
+  find: findAttachment,
   delete: deleteAttachment
 }
