16be7576b391a1008026d269b65d240356fd6869
Felix Michel <felix.michel@tum.de>
2017-03-17 20:19:40 +0100

    getBoxesByUser fix

--- server/models/box.model.js
+++ server/models/box.model.js
@@ -76,37 +76,21 @@ BoxSchema.statics.sortByLevel = function (boxes, user) {
 
 
 BoxSchema.statics.getBoxesByUser = (userId) => {
-  console.log('inside getBoxesByUser');
   return new Promise((resolve, reject) => {
     /*<boxId, box>*/
     const boxMap = new Map();
     Box.find({user: userId})
       .then(boxes => {
-        console.log(boxes);
         boxes.forEach(box => {
           box.unseen = 0;
-          if (box._id == '58c7b6e9116d2e549a316650') {
-            console.log('fasdfsdfa');
-            console.log(box);
-          }
-          boxMap.set(box._id, box);
+          boxMap.set(box._id+'', box);
         });
         return calcUnseenMessages(userId);
       })
       .then(unseenBoxes => {
-        console.log('unseen');
-        console.log(unseenBoxes);
         /* merge unseen count with boxes*/
         unseenBoxes.forEach(box => {
-          console.log('HERE the map');
-          console.log(boxMap);
-          console.log(boxMap.keys());
-          console.log('here the box info');
-          console.log(box);
-          console.log(box._id);
-          console.log('why undefined?');
-          console.log(boxMap.get(box._id));
-          boxMap.get(box._id).unseen = box.unseen;
+          boxMap.get(box._id+'').unseen = box.unseen;
         });
         resolve(Array.from(boxMap.values()));
       })
@@ -117,7 +101,7 @@ BoxSchema.statics.getBoxesByUser = (userId) => {
 
   function calcUnseenMessages(userId) {
     return Email.aggregate([
-      {$match: {flags: {$ne: "\\Seen"}, user: userId}}, //TODO check if unssen is the right flag
+      {$match: {flags: {$ne: "\\Seen"}, user: userId}},
       {$group: {_id: '$box', unseen: {$sum: 1}}}
     ]);
   }
