387dbcd1bdeaf10a869bac8027d2969e5e6d5473
Felix Michel <felix.michel@tum.de>
2017-03-26 08:02:15 +0200

    clean up

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -240,9 +240,7 @@ function syncIMAPBoxes(user, emailConnector) {
           return new Promise((resolve, reject)=>{
             Box.updateAndGetOldAndUpdated(box, user)
               .spread((oldBox, updatedBox) => {
-                console.log('here')
                 Socket.pushBoxUpdateToClient(oldBox, updatedBox);
-                console.log('here2')
                 resolve()
               })
               .catch(err => {
