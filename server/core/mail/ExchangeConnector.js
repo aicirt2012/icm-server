3b7d708834804aaa5b6c85f8fb0fd9b602a5ed08
Paul Gualotuna <paul.gualotuna@tum.de>
2017-04-03 13:03:30 +0200

    WIP: Exchange, get emails

--- server/core/mail/ExchangeConnector.js
+++ server/core/mail/ExchangeConnector.js
@@ -7,6 +7,8 @@ import ImapConnector from './ImapConnector';
 
 class ExchangeConnector extends ImapConnector {
 
+  // create exclude array
+
   constructor(options, user) {
     super(options, user);
   }
@@ -18,59 +20,69 @@ class ExchangeConnector extends ImapConnector {
       Promise.each(boxes, (box) => {
         console.log('inside the promise');
         console.log(box);
-        return this.fetchEmails(storeEmail, box).then((hms) => {
-          highestmodseq.push(hms);
-        });
-      }).then(() => {
-        this.user.highestmodseq = this.user.highestmodseq && parseInt(this.user.highestmodseq) > parseInt(highestmodseq[0]) ? this.user.highestmodseq : highestmodseq[0];
-        this.user.lastSync = new Date();
-        this.user.save().then(() => {
-          this.end().then(() => {
-            resolve();
+        const boxName = box.name;
+        console.log(boxName);
+        if (boxName == 'INBOX') {
+          return this.fetchEmails(storeEmail, box).then((hms) => {
+            highestmodseq.push(hms);
           });
+        } else {
+          return 'invalid box';
+        }
+      })
+        .then(() => {
+          this.user.highestmodseq = this.user.highestmodseq && parseInt(this.user.highestmodseq) > parseInt(highestmodseq[0]) ? this.user.highestmodseq : highestmodseq[0];
+          this.user.lastSync = new Date();
+          this.user.save().then(() => {
+            this.end().then(() => {
+              resolve();
+            });
+          });
+        })
+        .catch((error) => {
+          console.error('Error: ', error.message);
         });
-      }).catch(reject)
-    })) 
+    }))
   }
 
   fetchEmails(storeEmail, newBox) {
     console.log('inside fetchEmails');
     console.log
     return this.openBox(newBox.name).then((box) => {
-        return new Promise((resolve, reject) => {
-          let options = {
-            bodies: '',
-            struct: true,
-            markSeen: false
+      return new Promise((resolve, reject) => {
+        let options = {
+          bodies: '',
+          struct: true,
+          markSeen: false
+        };
+
+        if (this.user.highestmodseq) {
+          options.modifiers = {
+            changedsince: this.user.highestmodseq
           };
+        }
 
-          if (this.user.highestmodseq) {
-            options.modifiers = {
-              changedsince: this.user.highestmodseq
-            };
-          }
+        const f = this.imap.seq.fetch('1:*', options);
 
-          const f = this.imap.seq.fetch('1:*', options);
+        let promises = [];
 
-          let promises = [];
+        f.on('message', (mail, seqno) => {
+          promises.push(this.parseDataFromEmail(mail, newBox, storeEmail));
+        });
 
-          f.on('message', (mail, seqno) => {
-            promises.push(this.parseDataFromEmail(mail, newBox, storeEmail));
-          });
+        f.once('error', (err) => {
+          console.log('Fetch error: ', err);
+          this.end().then(reject);
+        });
 
-          f.once('error', (err) => {
-            console.log('Fetch error: ', err);
-            this.end().then(reject);
-          });
-
-          f.once('end', () => {
-            console.log('Done fetching all messages!');
-            Promise.all(promises).then(() => {
-              resolve(box.highestmodseq);
-            });
+        f.once('end', () => {
+          console.log('Done fetching all messages!');
+          Promise.all(promises).then(() => {
+            resolve(box.highestmodseq);
           });
         });
-      })
+      });
+    })
       .catch((error) => {
         console.error('Error: ', error.message);
       });
@@ -83,19 +95,23 @@ class ExchangeConnector extends ImapConnector {
 
       mailParser.on('end', (mailObject) => {
         console.log('inside parseDataFromEmail');
-        console.log(mailObject);
+        console.log(mailObject.from);
         mailObject.html = mailObject.html && mailObject.html.includes('<body') ? mailObject.html.substring(mailObject.html.indexOf('<body')) : mailObject.html;
 
         const email = {
           messageId: mailObject.messageId,
-          //attrs: mailObject,
-          thrid: mailObject.headers['thread-index'],
           from: mailObject.from,
           to: mailObject.to,
-          subject: mailObject.headers.subject,
-          html: mailObject.html,
+          subject: mailObject.subject,
           text: mailObject.text,
+          html: mailObject.html,
           date: mailObject.headers.date,
+          flags: attributes.flags,
+          //labels: attributes,
+          uid: attributes.uid,
+          //attrs: attributes, // recursive... so fails
+          thrid: mailObject.headers['thread-index'],
+          box: box._id,
           user: this.user
         };
         storeEmail(email).then((msg) => {
@@ -114,6 +130,8 @@ class ExchangeConnector extends ImapConnector {
         });
       }).once('attributes', (attrs) => {
         attributes = attrs;
+        console.log('these are the attributes');
+        console.log(attributes);
       }).once('end', () => {
         mailParser.end();
       }).on('error', (err) => reject(err));
