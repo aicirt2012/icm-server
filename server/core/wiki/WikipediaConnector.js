53174599c81df319de853285b247888d6e84f5df
Simon Bönisch <github@sunborn.me>
2018-01-22 14:33:33 +0100

    auto formatted

--- server/core/wiki/WikipediaConnector.js
+++ server/core/wiki/WikipediaConnector.js
@@ -1,45 +1,45 @@
 import wikipedia from 'node-wikipedia';
 import fs from 'fs';
+
 var cheerio = require('cheerio');
 
 class WikipediaConnector {
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    getArticle(query) {
-        return new Promise((resolve, reject) => {
-            wikipedia.page.data(query, { content: true }, function(article) {
-                let $ = cheerio.load(article.text['*']);   
+  getArticle(query) {
+    return new Promise((resolve, reject) => {
+      wikipedia.page.data(query, {content: true}, function (article) {
+        let $ = cheerio.load(article.text['*']);
 
-                // find relevant teaser        
-                let fp = $('p');
-                let teaser = '<p>'+fp.html()+'</p>';
-                while(fp.next().get(0).name == 'p'){                
-                    fp = fp.next();
-                    teaser += '<p>'+fp.html()+'</p>';
-                }
-                teaser = teaser.replace(/<a/g,'<span class="wiki-link"').replace(/a>/g,'span>');
-            
-                // remove references
-                $ = cheerio.load(teaser);  
-                $('.reference').remove(); 
-                $('.update').remove(); 
-                $('.noprint').remove();        
-                $('a').removeAttr('href');               
-                teaser = $.html();
+        // find relevant teaser
+        let fp = $('p');
+        let teaser = '<p>' + fp.html() + '</p>';
+        while (fp.next().get(0).name == 'p') {
+          fp = fp.next();
+          teaser += '<p>' + fp.html() + '</p>';
+        }
+        teaser = teaser.replace(/<a/g, '<span class="wiki-link"').replace(/a>/g, 'span>');
 
-                resolve({
-                    title: article.title,
-                    teaser: teaser
-                });
-            }, function(err){
-                reject(err);
-            });
+        // remove references
+        $ = cheerio.load(teaser);
+        $('.reference').remove();
+        $('.update').remove();
+        $('.noprint').remove();
+        $('a').removeAttr('href');
+        teaser = $.html();
+
+        resolve({
+          title: article.title,
+          teaser: teaser
         });
+      }, function (err) {
+        reject(err);
+      });
+    });
 
-    }
-
+  }
 
 
 }
