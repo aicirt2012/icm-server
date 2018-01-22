4d57eb478f15b0039f4192ab0147857450b505b3
Simon Bönisch <github@sunborn.me>
2018-01-22 14:39:22 +0100

    fixed server error when no suitable articles are found in wikipedia

--- server/core/wiki/WikipediaConnector.js
+++ server/core/wiki/WikipediaConnector.js
@@ -11,29 +11,33 @@ class WikipediaConnector {
   getArticle(query) {
     return new Promise((resolve, reject) => {
       wikipedia.page.data(query, {content: true}, function (article) {
-        let $ = cheerio.load(article.text['*']);
+        if (article && article.text && article.text['*']) {
+          let $ = cheerio.load(article.text['*']);
 
-        // find relevant teaser
-        let fp = $('p');
-        let teaser = '<p>' + fp.html() + '</p>';
-        while (fp.next().get(0).name == 'p') {
-          fp = fp.next();
-          teaser += '<p>' + fp.html() + '</p>';
+          // find relevant teaser
+          let fp = $('p');
+          let teaser = '<p>' + fp.html() + '</p>';
+          while (fp.next().get(0).name == 'p') {
+            fp = fp.next();
+            teaser += '<p>' + fp.html() + '</p>';
+          }
+          teaser = teaser.replace(/<a/g, '<span class="wiki-link"').replace(/a>/g, 'span>');
+
+          // remove references
+          $ = cheerio.load(teaser);
+          $('.reference').remove();
+          $('.update').remove();
+          $('.noprint').remove();
+          $('a').removeAttr('href');
+          teaser = $.html();
+
+          resolve({
+            title: article.title,
+            teaser: teaser
+          });
+        } else {
+          resolve({});
         }
-        teaser = teaser.replace(/<a/g, '<span class="wiki-link"').replace(/a>/g, 'span>');
-
-        // remove references
-        $ = cheerio.load(teaser);
-        $('.reference').remove();
-        $('.update').remove();
-        $('.noprint').remove();
-        $('a').removeAttr('href');
-        teaser = $.html();
-
-        resolve({
-          title: article.title,
-          teaser: teaser
-        });
       }, function (err) {
         reject(err);
       });
