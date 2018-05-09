51d68703a831d283d6fd85fc843bab4ffb464578
Simon Bönisch <github@sunborn.me>
2018-05-09 17:06:49 +0200

    updated fields and field names

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,16 +1,23 @@
 import mongoose from 'mongoose';
-import mongoosePaginate from 'mongoose-paginate';
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const TaskSchema = new mongoose.Schema({
-  taskId: String,
+  taskId: String, // deprecated provider id, TODO fade-out this field
   provider: String,
-  email: {type: ObjectId, ref: 'Email'},
-  user: {type: ObjectId, ref: 'User'}, //TODO populate this field
-  thrid: String,
-  optionalParameters: [{name: String, type: String, constraints: String, value: Object, defaultValue: Object}],
-  requiredParameters: [{name: String, type: String, constraints: String, value: Object, defaultValue: Object}],
+  providerId: String,
+  user: {type: ObjectId, ref: 'User'}, //TODO check if field population necessary and implement
+  email: {type: ObjectId, ref: 'Email'}, //TODO check if field population necessary and implement
+  thrid: String, // deprecated thread id, TODO fade-out this field
+  threadId: String,
+  parameters: [{
+    name: String,
+    type: String,
+    constraints: String,
+    value: Object,
+    defaultValues: [Object],
+    isRequired: Boolean
+  }],
 }, {
   timestamps: true
 });
@@ -19,7 +26,7 @@ TaskSchema.method({});
 
 TaskSchema.statics.removeByUserId = (userId) => {
   return Task.find({user: userId}).remove().exec();
-}
+};
 
 let Task = mongoose.model('Task', TaskSchema);
 export default Task;
