82ea11c74601a0d3174325b1483bbfcd6f17bf6d
Simon Bönisch <github@sunborn.me>
2018-05-15 13:09:40 +0200

    updated task model to match jsons and not persist parameters array

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -3,21 +3,25 @@ import mongoose from 'mongoose';
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const TaskSchema = new mongoose.Schema({
-  taskId: String, // deprecated provider id, TODO fade-out this field
   provider: String,
   providerId: String,
   user: {type: ObjectId, ref: 'User'}, //TODO check if field population necessary and implement
-  email: {type: ObjectId, ref: 'Email'}, //TODO check if field population necessary and implement
-  thrid: String, // deprecated thread id, TODO fade-out this field
   threadId: String,
-  parameters: [{
-    name: String,
-    type: String,
-    constraints: String,
-    value: Object,
-    defaultValues: [Object],
-    isRequired: Boolean
-  }],
+  email: {type: ObjectId, ref: 'Email'}, //TODO check if field population necessary and implement
+
+  // --- deprecated fields, will be removed in future
+  taskId: String, // deprecated provider id, TODO fade-out this field
+  thrid: String, // deprecated thread id, TODO fade-out this field
+
+  // --- fields that are used but commented out on purpose so they don't get persisted to DB (mongoDB strict mode)
+  // parameters: [{
+  //   name: String,
+  //   type: String,
+  //   constraints: String,
+  //   value: Object,
+  //   defaultValues: [Object],
+  //   isRequired: Boolean
+  // }],
 }, {
   timestamps: true
 });
