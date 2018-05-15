ec6e23487e13917e00ad762b8a6c03847e46f4a8
Simon Bönisch <github@sunborn.me>
2018-05-15 23:37:31 +0200

    added method stubs to actual task services

--- server/core/task/sociocortex.service.js
+++ server/core/task/sociocortex.service.js
@@ -2,6 +2,34 @@ import TaskService from "./task.service";
 
 class SociocortexService extends TaskService {
 
+  configure(username, password, providerSpecificData) {
+    throw new Error("Not yet implemented!");
+  }
+
+  setup(providerSpecificData) {
+    throw new Error("Not yet implemented!");
+  }
+
+  teardown(providerSpecificData) {
+    throw new Error("Not yet implemented!");
+  }
+
+  create(task) {
+    throw new Error("Not yet implemented!");
+  }
+
+  get(provider_id) {
+    throw new Error("Not yet implemented!");
+  }
+
+  update(provider_id, task) {
+    throw new Error("Not yet implemented!");
+  }
+
+  delete(provider_id) {
+    throw new Error("Not yet implemented!");
+  }
+
 }
 
 export default SociocortexService;
