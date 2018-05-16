09feb907797514e5aa5e98ad9343a675fb4c144a
Simon Bönisch <github@sunborn.me>
2018-05-16 21:03:57 +0200

    reduced syntax complexity

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -6,9 +6,9 @@ import Task from "../models/task.model";
 exports.configure = (req, res) => {
   getTaskService(req.params.id, req.user)
     .configure(req.body.email, "", {})
-    .then((data) => {
+    .then(data => {
       res.status(200).send(data);
-    }).catch((err) => {
+    }).catch(err => {
     res.status(400).send(err);
   });
 };
@@ -16,9 +16,9 @@ exports.configure = (req, res) => {
 exports.setup = (req, res) => {
   getTaskService(req.params.id, req.user)
     .setup(req.body.token)
-    .then((data) => {
+    .then(data => {
       res.status(200).send(data);
-    }).catch((err) => {
+    }).catch(err => {
     res.status(400).send(err);
   });
 };
@@ -26,9 +26,9 @@ exports.setup = (req, res) => {
 exports.teardown = (req, res) => {
   getTaskService(req.params.id, req.user)
     .teardown(req.body)
-    .then((data) => {
+    .then(data => {
       res.status(200).send(data);
-    }).catch((err) => {
+    }).catch(err => {
     res.status(400).send(err);
   });
 };
@@ -36,60 +36,60 @@ exports.teardown = (req, res) => {
 exports.createTask = (req, res) => {
   getTaskService(req.body.provider, req.user)
     .create(req.body)
-    .then((task) => {
-      task.save().then((result) => {
-        res.status(200).send(result);
+    .then(task => {
+      task.save().then(task => {
+        res.status(200).send(task);
       })
-    }).catch((err) => {
+    }).catch(err => {
     res.status(400).send(err);
   });
 };
 
 exports.readTask = (req, res) => {
-  Task.findById(req.params.id).then((task) => {
+  Task.findById(req.params.id).then(task => {
     getTaskService(task.provider, req.user)
       .get(task.providerId)
-      .then((data) => {
-        res.status(200).send(data);
+      .then(task => {
+        res.status(200).send(task);
       });
-  }).catch((err) => {
+  }).catch(err => {
     res.status(400).send(err);
   });
 };
 
 exports.updateTask = (req, res) => {
   // load task from DB to ensure correct current provider is used
-  Task.findById(req.params.id).then((task) => {
+  Task.findById(req.params.id).then(task => {
     getTaskService(task.provider, req.user)
       .update(task.providerId, req.body)
-      .then((data) => {
-        res.status(200).send(data);
+      .then(task => {
+        res.status(200).send(task);
       });
-  }).catch((err) => {
+  }).catch(err => {
     res.status(400).send(err);
   });
 };
 
 exports.deleteTask = (req, res) => {
-  Task.findById(req.params.id).then((task) => {
+  Task.findById(req.params.id).then(task => {
     getTaskService(task.provider, req.user)
       .delete(task.providerId)
       .then(() => {
         Task.delete(req.params.id)
-          .then((data) => {
+          .then(data => {
             res.status(200).send(data);
           });
       });
-  }).catch((err) => {
+  }).catch(err => {
     res.status(400).send(err);
   });
 };
 
 exports.searchTasks = (req, res) => {
   // FIXME do not just pass request body to mongo, potential security risk, current implementation only for development
-  Task.find(req.body).then((tasks) => {
+  Task.find(req.body).then(tasks => {
     res.status(200).send(tasks);
-  }).catch((err) => {
+  }).catch(err => {
     res.status(400).send(err);
   });
 };
