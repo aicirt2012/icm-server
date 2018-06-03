e5e1969aa9b7c853baec04c9673b0fde9abc1f5c
Simon Bönisch <github@sunborn.me>
2018-06-03 18:41:40 +0200

    WIP: started implementing archiveTask

--- server/controllers/task.trello.controller.js
+++ server/controllers/task.trello.controller.js
@@ -1,4 +1,5 @@
 import TrelloService from "../core/task/trello.service";
+import Task from "../models/task.model";
 
 exports.listBoards = (req, res) => {
   new TrelloService(req.user)
@@ -11,11 +12,17 @@ exports.listBoards = (req, res) => {
 };
 
 exports.archiveTask = (req, res) => {
-  // TODO implement (simple PUT with closed=true)
-  new TrelloService(req.user)
-    .listBoards()
-    .then(boards => {
-      res.status(200).send(boards);
+  Task.findOne({_id: req.params.id, user: req.user._id})
+    .then(task => {
+      new TrelloService(req.user)
+        .update(task.providerId, req.body)
+        .then(providerTask => {
+          task = task.toObject();
+          task.parameters = providerTask.parameters;
+          res.status(200).send(task);
+        }).catch(err => {
+        res.status(400).send(err);
+      });
     }).catch(err => {
     res.status(400).send(err);
   });
