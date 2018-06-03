79472315a688cb998d29a63b40f834348b23bfa2
Simon Bönisch <github@sunborn.me>
2018-06-04 01:20:46 +0200

    implemented trello task dis- and reassembling

--- server/core/task/assembler/trello.assembler.js
+++ server/core/task/assembler/trello.assembler.js
@@ -1,12 +1,38 @@
+import Constants from "../../../../config/constants";
+import Task from "../../../models/task.model";
+
 class TrelloAssembler {
 
   static Task = new class extends AbstractAssembler {
-    fromExternalObject(externalObject) {
-      throw new Error("Not yet implemented: Method 'fromExternalObject' for Tasks.");
+    fromExternalObject(trelloTask) {
+      const task = {};
+      task.provider = Constants.taskProviders.trello;
+      task.providerId = trelloTask.id;
+      task.parameters = [
+        {name: 'name', value: trelloTask.name},
+        {name: 'desc', value: trelloTask.desc},
+        {name: 'due', value: trelloTask.due},
+        {name: 'closed', value: trelloTask.closed},
+        {name: 'idBoard', value: trelloTask.idBoard},
+        {name: 'idList', value: trelloTask.idList},
+        {name: 'idMembers', value: trelloTask.idMembers},
+        {name: 'shortUrl', value: trelloTask.shortUrl}
+      ];
+      return task;
     }
 
-    toExternalObject(internalObject) {
-      throw new Error("Not yet implemented: Method 'toExternalObject' for Tasks.");
+    toExternalObject(task) {
+      const trelloTask = {};
+      trelloTask.id = task.providerId;
+      trelloTask.name = Task.getParameterValue(task.parameters, 'name');
+      trelloTask.desc = Task.getParameterValue(task.parameters, 'desc');
+      trelloTask.due = Task.getParameterValue(task.parameters, 'due');
+      trelloTask.closed = Task.getParameterValue(task.parameters, 'closed');
+      trelloTask.idBoard = Task.getParameterValue(task.parameters, 'idBoard');
+      trelloTask.idList = Task.getParameterValue(task.parameters, 'idList');
+      trelloTask.idMembers = Task.getParameterValue(task.parameters, 'idMembers');
+      trelloTask.shortUrl = Task.getParameterValue(task.parameters, 'shortUrl');
+      return trelloTask;
     }
   }();
 
