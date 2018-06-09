941e9f0a666505842e2cb3c42431eeea7f3275d3
Simon Bönisch <github@sunborn.me>
2018-06-09 03:20:21 +0200

    removed deprecated, unused files

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -8,8 +8,6 @@ import GmailConnector from '../core/mail/GmailConnector';
 import EWSConnector from '../core/mail/EWSConnector';
 import Pattern from '../models/pattern.model'
 import Constants from '../../config/constants';
-import {createTaskConnector} from '../core/task/util';
-import TaskServiceUtil from "../core/task/TaskServiceUtil";
 
 
 exports.sendEmail = (req, res) => {
@@ -314,7 +312,9 @@ exports.getSingleMail = (req, res) => {
     .then((mail) => {
       // replace attachments
       mail = replaceInlineAttachmentsSrc(mail, req.user);
-      return (mail && (req.user.trello || req.user.sociocortex)) ? TaskServiceUtil.addLinkedTasksToEmail(mail, req.user) : mail;
+      return mail;
+      // TODO load and attach tasks linked to the email
+      // return (mail && (req.user.trello || req.user.sociocortex)) ? TaskServiceUtil.addLinkedTasksToEmail(mail, req.user) : mail;
     })
     .then(mail => {
       // get all patterns for current user
@@ -345,22 +345,23 @@ exports.getSingleMail = (req, res) => {
           fullName: x.formattedValue,
         })
       );
-      // TODO move the following trello interaction to taskService
-      return !(email && req.user.trello) ? mail : createTaskConnector(Constants.taskProviders.trello, req.user)
-        .getOpenBoardsForMember({}).then(allBoards => {
-          return getUserFullNamesFromEmail(email, req.user).then(emails => {
-            //put people in to, cc,from, bcc first
-            allPersons = allPersons.concat(emails, allPersons);
-            let recognizedPersons = getMentionedPersons(allPersons, allBoards);
-            email['suggestedData'] = {
-              titles: resultDTO.annotations.filter(x => x.nerType === Constants.nerTypes.taskTitle).map(x => x.formattedValue),
-              dates: resultDTO.annotations.filter(x => x.nerType === Constants.nerTypes.date).map(x => x.formattedValue).sort((a, b) => new Date(b) - new Date(a)),
-              persons: recognizedPersons
-            };
-            email['suggestedData']['titles'].push(email.subject);
-            return email;
-          })
-        });
+      return email;
+      // // TODO move the following trello interaction to taskService
+      // return !(email && req.user.trello) ? mail : createTaskConnector(Constants.taskProviders.trello, req.user)
+      //   .getOpenBoardsForMember({}).then(allBoards => {
+      //     return getUserFullNamesFromEmail(email, req.user).then(emails => {
+      //       //put people in to, cc,from, bcc first
+      //       allPersons = allPersons.concat(emails, allPersons);
+      //       let recognizedPersons = getMentionedPersons(allPersons, allBoards);
+      //       email['suggestedData'] = {
+      //         titles: resultDTO.annotations.filter(x => x.nerType === Constants.nerTypes.taskTitle).map(x => x.formattedValue),
+      //         dates: resultDTO.annotations.filter(x => x.nerType === Constants.nerTypes.date).map(x => x.formattedValue).sort((a, b) => new Date(b) - new Date(a)),
+      //         persons: recognizedPersons
+      //       };
+      //       email['suggestedData']['titles'].push(email.subject);
+      //       return email;
+      //     })
+      //   });
     })
     .then(email => {
       res.status(200).send(email);
@@ -376,48 +377,48 @@ exports.getSingleMail = (req, res) => {
 };
 
 
-function getMentionedPersons(nerPersons, trelloBoards) {
-  let result = [];
-  nerPersons.forEach(item => {
-    trelloBoards.forEach(board => {
-      board.members.forEach(member => {
-        if ((member.fullName.includes(item.fullName) || member.username.includes(item.fullName)) &&
-          result.findIndex(existingItem => existingItem.username === member.username) === -1)
-          result.push(TaskServiceUtil.convertMemberToMinimalEntity(member));
-      })
-    })
-  });
-  return result;
-}
-
-
-function getUserFullNamesFromEmail(email, user) {
-  let allAddresses = email.to;
-  allAddresses = allAddresses.concat(email.cc);
-  allAddresses = allAddresses.concat(email.from);
-  allAddresses = allAddresses.concat(email.bcc);
-  let trelloConnector = createTaskConnector(Constants.taskProviders.trello, user);
-
-  let result = [];
-  let promises = [];
-  return new Promise((resolve, reject) => {
-    allAddresses.forEach(address => {
-        let newPromise = trelloConnector.searchMembers({query: address.address, limit: '1'});
-        promises.push(newPromise);
-      }
-    );
-    Promise.all(promises).then((values) => {
-      console.log(values);
-      values.forEach(value => {
-        if (value.length > 0 && value[0].fullName && result.findIndex(existingItem => existingItem.fullName === value[0].fullName) === -1)
-          result.push({"fullName": value[0].fullName});
-      });
-      resolve(result);
-    }).catch((err) => {
-      reject(err);
-    });
-  });
-}
+// function getMentionedPersons(nerPersons, trelloBoards) {
+//   let result = [];
+//   nerPersons.forEach(item => {
+//     trelloBoards.forEach(board => {
+//       board.members.forEach(member => {
+//         if ((member.fullName.includes(item.fullName) || member.username.includes(item.fullName)) &&
+//           result.findIndex(existingItem => existingItem.username === member.username) === -1)
+//           result.push(TaskServiceUtil.convertMemberToMinimalEntity(member));
+//       })
+//     })
+//   });
+//   return result;
+// }
+//
+//
+// function getUserFullNamesFromEmail(email, user) {
+//   let allAddresses = email.to;
+//   allAddresses = allAddresses.concat(email.cc);
+//   allAddresses = allAddresses.concat(email.from);
+//   allAddresses = allAddresses.concat(email.bcc);
+//   let trelloConnector = createTaskConnector(Constants.taskProviders.trello, user);
+//
+//   let result = [];
+//   let promises = [];
+//   return new Promise((resolve, reject) => {
+//     allAddresses.forEach(address => {
+//         let newPromise = trelloConnector.searchMembers({query: address.address, limit: '1'});
+//         promises.push(newPromise);
+//       }
+//     );
+//     Promise.all(promises).then((values) => {
+//       console.log(values);
+//       values.forEach(value => {
+//         if (value.length > 0 && value[0].fullName && result.findIndex(existingItem => existingItem.fullName === value[0].fullName) === -1)
+//           result.push({"fullName": value[0].fullName});
+//       });
+//       resolve(result);
+//     }).catch((err) => {
+//       reject(err);
+//     });
+//   });
+// }
 
 
 // Inline attachments URL and tokens are changed in the front-end
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import express from 'express';
 import validate from 'express-validation';
 import paramValidation from '../../config/param-validation';
@@ -7,7 +8,6 @@ import importCtrl from '../controllers/import.controller';
 import emailCtrl from '../controllers/email.controller';
 import boxCtrl from '../controllers/box.controller';
 import attachmentCtrl from '../controllers/attachment.controller';
-import taskLegacyCtrl from '../controllers/task.legacy.controller';   // TODO remove this import and fix resulting compile errors
 import taskCtrl from '../controllers/task.controller'
 import taskTrelloCtrl from '../controllers/task.trello.controller'
 import taskSociocortexCtrl from '../controllers/task.sociocortex.controller'
