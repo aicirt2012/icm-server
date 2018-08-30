837ae482c1605ff8bea02d6aea51c9624cc47394
Simon Bönisch <github@sunborn.me>
2018-08-30 19:19:51 +0200

    re-formatted

--- server/controllers/task.trello.controller.js
+++ server/controllers/task.trello.controller.js
@@ -5,50 +5,33 @@ import TaskService from "../core/task/task.service";
 exports.listBoards = (req, res) => {
   new TrelloService(req.user)
     .listBoards()
-    .then(boards => {
-      res.status(200).send(boards);
-    }).catch(err => {
-    res.status(400).send(err);
-  });
+    .then(boards => res.status(200).send(boards))
+    .catch(err => res.status(400).send(err));
 };
 
 exports.archiveTask = (req, res) => {
   const trelloService = new TrelloService(req.user);
   Task.findOne({_id: req.params.id, user: req.user._id})
-    .then(icmTask => {
-      trelloService.get(icmTask.providerId)
-        .then(task => {
-          task.isOpen = false;
-          trelloService.update(icmTask.providerId, task)
-            .then(providerTask => {
-              res.status(200).send(TaskService.mergeTaskObjects(icmTask, providerTask));
-            }).catch(err => {
-            res.status(400).send(err);
-          });
-        }).catch(err => {
-        res.status(400).send(err);
-      });
-    }).catch(err => {
-    res.status(400).send(err);
-  });
+    .then(icmTask => trelloService.get(icmTask.providerId)
+      .then(task => {
+        task.isOpen = false;
+        trelloService.update(icmTask.providerId, task)
+          .then(providerTask => res.status(200).send(TaskService.mergeTaskObjects(icmTask, providerTask)))
+          .catch(err => res.status(400).send(err));
+      }).catch(err => res.status(400).send(err)))
+    .catch(err => res.status(400).send(err));
 };
 
 exports.listMembers = (req, res) => {
   new TrelloService(req.user)
     .getMembers(req.params.id)
-    .then(members => {
-      res.status(200).send(members);
-    }).catch(err => {
-    res.status(400).send(err);
-  });
+    .then(members => res.status(200).send(members))
+    .catch(err => res.status(400).send(err));
 };
 
 exports.getTasks = (req, res) => {
   new TrelloService(req.user)
     .getTasks(req.params.id)
-    .then(members => {
-      res.status(200).send(members);
-    }).catch(err => {
-    res.status(400).send(err);
-  });
+    .then(tasks => res.status(200).send(tasks))
+    .catch(err => res.status(400).send(err));
 };
